fd4c0ef81d4785b4aa82fa0c13ee58a09a643b20
ZombiBananaDev <69401732+ZombiDebugWall@users.noreply.github.com>
2022-08-08 20:54:49 +0200

    Add files via upload

---
+++ mainpqthread.h
@@ -0,0 +1,25 @@
+
+#pragma once
+
+
+namespace PQGlobVar
+{
+    long double *amount_of_runsptr;
+    long double *iptr;
+    bool finished = false;
+    
+    /*const char * progressbar = " ";
+    const char ** bar;
+    double logvalue_left;
+    double logvalue_right;
+    double sqrlogvalue_right;
+    double sqrlogvalue_left;*/
+};
+
+
+
+
+
+
+
+
